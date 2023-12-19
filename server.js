19ae3b9553e861da9e57644e65d30a70109a7aa5
David <74427665+davidsvida@users.noreply.github.com>
2023-12-19 16:09:36 -0600

    deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server')
 
+require('dotenv').config()
+
 const { sequelize } = require('./models')
 
 const resolvers = require('./graphql/resolvers')
@@ -10,10 +12,12 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: contextMiddleware,
+  subscriptions: { path: '/' },
 })
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+server.listen().then(({ url, subscriptionsUrl }) => {
+  console.log(`Server ready at ${url}`)
+  console.log(`Subscriptions ready at ${subscriptionsUrl}}`)
 
   sequelize
     .authenticate()

8c1204396c2f18b156436e00558fa7d3a0e29df3
David <74427665+davidsvida@users.noreply.github.com>
2023-12-19 15:49:31 -0600

    first commit

---
+++ models/user.js
@@ -0,0 +1,41 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    
+    static associate(models) {
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'must be a valid email address',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imageUrl: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+    }
+  )
+
+  return User
+}
